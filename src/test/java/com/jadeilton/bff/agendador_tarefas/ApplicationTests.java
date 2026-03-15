3b764d153dbf0ec87e50634d75f981f679aadfea
Jadeilton <jadeiltondm@gmail.com>
2026-03-15 16:56:59 -0300

    Adicionado esqueleto do projeto

---
+++ src/test/java/com/jadeilton/bff/agendador_tarefas/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jadeilton.bff.agendador_tarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
